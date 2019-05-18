d3bea8b3900f4c596dfa3442e369777492267360
Sepehr Madani <ssepehrmadani@gmail.com>
2019-05-17 22:33:22 -0400

    Revert "added timer + cleanup"
    This reverts commit e046625b

--- Runner.java
+++ Runner.java
@@ -6,31 +6,39 @@ public class Runner {
 	public static void main(String[] args) {
 		Puzzle puzzle = new Puzzle();
 		String puzzlePath = new File("").getAbsolutePath();
-		puzzlePath = puzzlePath.concat("\\src\\Hard.txt");
+		puzzlePath = puzzlePath.concat("\\src\\Easy.txt");
 		File PuzzleFile = new File(puzzlePath);
 		try {
+			Scanner nextIteration = new Scanner(System.in);
 			Scanner PuzzleReader = new Scanner(PuzzleFile);
-			for (int i = 0; i < 9; i++)
-				for (int j = 0; j < 9; j++)
+			for (int i = 0; i < 9; i++) {
+				for (int j = 0; j < 9; j++) {
 					puzzle.board[i][j].value = PuzzleReader.nextInt();
+				}
+			}
+
 			System.out.println("INITIAL PUZZLE");
+			int attempt = 0;
 			puzzle.display();
-			System.out.println("PROCESSING...");
-			long startTime = System.nanoTime();
 			while(!puzzle.isComplete()) {
-				puzzle.iterate();
-				if (!puzzle.hasTheBoardChanged) {
+				puzzle.updateBlacklist();
+				puzzle.refillWithBlacklist();
+
+				if (puzzle.hasTheBoardChanged) {
+					attempt++;
+					System.out.println("ATTEMPT " + attempt);
 					puzzle.display();
-					System.out.println("STUCK :(");
+				} else {
+					System.out.println("STUCK");
 					System.exit(0);
 				}
 			}
-			long endTime = System.nanoTime();
-			long duration = (endTime - startTime);
-			int ms = (int) (duration / 1000000);
-			puzzle.display();
-			System.out.println("PUZZLE SOLVED IN " + ms + "ms.");
+			System.out.println("PUZZLE SOLVED.");
 			PuzzleReader.close();
-		} catch (FileNotFoundException e) { e.printStackTrace(); }
+			nextIteration.close();
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
