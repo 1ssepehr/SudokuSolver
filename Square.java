d8e577396154bc07ea0c37cb77752e3e676cc165
Sepehr Madani <ssepehrmadani@gmail.com>
2019-05-17 22:25:53 -0400

    cleaned up the methods

--- Square.java
+++ Square.java
@@ -1,22 +1,31 @@
 class Square {
 	int value;
 	boolean[] blacklist;
-	Region region;
+	subGrid subGrid;
 
 	Square() {
-		blacklist = new boolean[] {false, false, false, false, false, false, false, false, false, false};
+		blacklist = new boolean[] {true, false, false, false, false, false, false, false, false, false};
 		this.value = 0;
 	}
 
-	boolean hasOnePossibleNumber() {
+	boolean isEmpty() {
+		return (value == 0);
+	}
+
+	int howManyPossible() {
 		int counter = 0;
 		for (int i = 1; i < 10; i++)
 			if(blacklist[i])
 				counter++;
-		return (counter == 8);
+		return 9 - counter;
+	}
+
+	void ban(int n) {
+		blacklist[n] = true;
 	}
 
 	int getPossibleNumber() {
+		assert howManyPossible() == 1;
 		for(int i = 1; i < 10; i++)
 			if(!blacklist[i])
 				return i;
